4d9f9d9c08785f3a3edc5eec40630d86c505ea3a
Armaankhaan01 <armankhan876426@gmail.com>
2025-07-30 11:58:44 +0530

    error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Providers from "./providers";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Analytics from "@/components/Analytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
